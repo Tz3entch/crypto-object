ca3f2d322591a54dcefa268a22ff8a9b97f5a4dd
Sergey Mozgov <svmozgov@yandex.ru>
2017-12-09 23:16:29 +0300

    tests fixed

--- test/index.js
+++ test/index.js
@@ -1,7 +1,6 @@
 /* eslint no-unused-vars: 0 */
 /* eslint no-unused-expressions: 0 */
 /* eslint no-undef: 0 */
-
 'use strict';
 
 const chai = require('chai');
@@ -13,23 +12,30 @@ const expect = chai.expect;
 describe('Basic functionality & logic flow', () => {
   it(`Should instantiate`, () => {
     const cryptoObject = new CryptoObject({
-      password: 'monica',
+      key: '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+      iv: '60iP0h6vJoEaarvg',
       keys: ['foo']
     });
     expect(cryptoObject).to.be.ok;
   });
 
-  it('Should throw an exception if no password is provided', () => {
+  it('Should throw an exception if no key is provided', () => {
     expect(() => new CryptoObject()).to.throw();
   });
 
+  it('Should throw an exception if no iv is provided', () => {
+    expect(() => new CryptoObject({key: '3zTvzr3p67VC61jmV54rIYu1545x4TlY'})).to.throw();
+  });
+
   it('Should throw an exception if no keys are provided', () => {
-    expect(() => new CryptoObject({password: 'monica'})).to.throw();
+    expect(() => new CryptoObject({key: '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+      iv: '60iP0h6vJoEaarvg'})).to.throw();
   });
 
   it('Should only encrypt specified keys', () => {
     const cryptoObject = new CryptoObject({
-      password: 'monica',
+      key: '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+      iv: '60iP0h6vJoEaarvg',
       keys: ['foo1', 'foo3']
     });
 
@@ -56,7 +62,7 @@ describe('Basic functionality & logic flow', () => {
     decryptedObject.should.deep.equal(plainObject);
   });
 
-  it('Should not decrypt if decryption password is different than encryption password', () => {
+  it('Should not decrypt if decryption key is different than encryption key', () => {
     let cryptoObject = {};
 
     const plainObject = {
@@ -64,13 +70,45 @@ describe('Basic functionality & logic flow', () => {
     };
 
     cryptoObject = new CryptoObject({
-      password: 'monica',
+      key: '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+      iv: '60iP0h6vJoEaarvg',
       keys: ['foo']
     });
     const cipherObject = cryptoObject.encrypt(plainObject);
 
     cryptoObject = new CryptoObject({
-      password: 'a-wrong-password',
+      key: '3zTvzr3p67VC61jmV54rIYu1545x4Tl1',
+      iv: '60iP0h6vJoEaarvg',
+      keys: ['foo']
+    });
+    const decryptedObject = cryptoObject.decrypt(cipherObject);
+
+    cipherObject.should.be.an('Object');
+    decryptedObject.should.be.an('Object');
+
+    cipherObject.should.not.deep.equal(plainObject);
+    cipherObject.should.not.deep.equal(decryptedObject);
+
+    decryptedObject.should.not.deep.equal(plainObject);
+  });
+
+  it('Should not decrypt if decryption iv is different than encryption iv', () => {
+    let cryptoObject = {};
+
+    const plainObject = {
+      foo: 'bar'
+    };
+
+    cryptoObject = new CryptoObject({
+      key: '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+      iv: '60iP0h6vJoEaarvg',
+      keys: ['foo']
+    });
+    const cipherObject = cryptoObject.encrypt(plainObject);
+
+    cryptoObject = new CryptoObject({
+      key: '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+      iv: '60iP0h6vJoEaarvz',
       keys: ['foo']
     });
     const decryptedObject = cryptoObject.decrypt(cipherObject);
@@ -88,7 +126,8 @@ describe('Basic functionality & logic flow', () => {
 describe('Tolerance of plaintext', () => {
   it('Should encrypt alphanumeric key values', () => {
     const cryptoObject = new CryptoObject({
-      password: 'monica',
+      key: '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+      iv: '60iP0h6vJoEaarvg',
       keys: ['foo']
     });
 
@@ -110,7 +149,8 @@ describe('Tolerance of plaintext', () => {
 
   it('Should encrypt non-ASCII key values - (Arabic Text)', () => {
     const cryptoObject = new CryptoObject({
-      password: 'monica',
+      key: '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+      iv: '60iP0h6vJoEaarvg',
       keys: ['foo']
     });
 
@@ -132,7 +172,8 @@ describe('Tolerance of plaintext', () => {
 
   it('Should encrypt long key values - (> 1500 chars)', () => {
     const cryptoObject = new CryptoObject({
-      password: 'monica',
+      key: '3zTvzr3p67VC61jmV54rIYu1545x4TlY',
+      iv: '60iP0h6vJoEaarvg',
       keys: ['foo']
     });
 
